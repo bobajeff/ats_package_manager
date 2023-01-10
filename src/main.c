44162d0bc519e5e4649da91f9de09dbb283f7bb1
Jeffrey Brusseau <bobajeff82@gmail.com>
2023-01-10 10:36:49 -0600

    search src/bin for folders containing main.dats

--- src/main.c
+++ src/main.c
@@ -19,16 +19,42 @@ int search_package_for_binaries(char *available_binaries[]) {
   int bin_count = 0;
   DIR *d;
   struct dirent *dir;
-
   d = opendir("src/bin/");
+
+  // read manifest.toml to find name of the project
+  // check for src/main.dats
+  // if doesn't exist don't use for default binary
+
   if (d) {
     while ((dir = readdir(d)) != NULL) {
       // gaurd against going over the max size of available_binaries array
       if (offset > 10) {
         break;
       }
+      // find folders containing main.dats file
+      if (dir->d_type == DT_DIR && strcmp(dir->d_name, ".") != 0 &&
+          strcmp(dir->d_name, "..") != 0) {
+        printf("\n%s is a directory\n\n", dir->d_name);
+        // check if directory has main.dats
+        FILE *file;
+        char *fileuri = malloc(strlen("src/bin/") + strlen(dir->d_name) +
+                               strlen("/main.dats") + 1);
+        strcpy(fileuri, "src/bin/");
+        strcat(fileuri, dir->d_name);
+        strcat(fileuri, "/main.dats");
+
+        if ((file = fopen(fileuri, "r")) != NULL) {
+          printf("%s has main.dats", dir->d_name);
+          // add directory name to binaries
+          available_binaries[offset++] = dir->d_name;
+          bin_count++;
+          fclose(file);
+        }
+        free(fileuri);
+
+      }
       // find .dats files
-      if (strstr(dir->d_name, ".dats") != NULL) {
+      else if (strstr(dir->d_name, ".dats") != NULL) {
         // extract the stem from the filename
         char *pointer_to_end_of_stem = strstr(dir->d_name, ".dats");
         if (pointer_to_end_of_stem != NULL) {
